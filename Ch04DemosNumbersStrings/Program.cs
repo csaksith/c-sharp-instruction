f4eeba156aa0260fe3427f0703cbe8071f75a112
csaksith <saksitca@mail.uc.edu>
2025-02-01 18:52:47 -0500

    Add C# examples on arithmetic

--- Ch04DemosNumbersStrings/Program.cs
+++ Ch04DemosNumbersStrings/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to the Number and Strings Demos!");
 int dayOfWeek = 5;
-double price = 11.99m;
+decimal price = 11.99m;
 int quantity = 3;
 char letter = 'A';
 bool isValid = false;
@@ -15,8 +15,27 @@ const int DaysInNovember = 30;
 //arithmetic operators
 decimal lineItemTotal = price*quantity;
 int count = 0;
+Console.WriteLine(count);
 count = count + 1;
 Console.WriteLine("count = " + count);
+count++;
+Console.WriteLine("count = "+count);
+
+int a = 9;
+int b = 2;
+int quotient = a/b;
+int remainder = a % b;
+Console.WriteLine("quotient = "+quotient);
+Console.WriteLine("reminder = " + remainder);
+
+double result = a / b;
+
+double c = 9.0;
+double d = 2.0;
+//decimal math
+result = c / d;
+
+Console.WriteLine("result = " + result);
 
 
 Console.WriteLine("Goodbye");
