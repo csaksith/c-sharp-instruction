e39e3955aa8db7e6d851ed223c267f6d50457bfa
csaksith <saksitca@mail.uc.edu>
2025-02-01 18:00:04 -0500

    add ch4 demos and practice

---
+++ Ch04DemosNumbersStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the Number and Strings Demos!");
+int dayOfWeek = 5;
+double price = 11.99m;
+int quantity = 3;
+char letter = 'A';
+bool isValid = false;
+quantity = 'b';
+double interestRate = .0367;
+
+//constants
+const int DaysInWeek = 7;
+const int DaysInNovember = 30;
+
+//arithmetic operators
+decimal lineItemTotal = price*quantity;
+int count = 0;
+count = count + 1;
+Console.WriteLine("count = " + count);
+
+
+Console.WriteLine("Goodbye");
+
---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Happy Friyay!!");
+            Console.WriteLine ('x');
+            Console.WriteLine(45);
+            Console.WriteLine(11.11);
+            Console.WriteLine(.212);
+        }
+    }
+}
