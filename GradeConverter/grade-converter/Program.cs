c02740f1ff9e9554688a27873bc7fc4c1476ea47
csaksith <saksitca@mail.uc.edu>
2025-02-02 11:55:56 -0500

    complete grade converter exercise

---
+++ GradeConverter/grade-converter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+Console.WriteLine("Welcome to the Letter Grade Converter");
+string choice = "y";
+while (choice == "y") 
+
+{
+Console.WriteLine("Enter numerical grade: ");
+int grade;
+
+//validate input
+ while (!int.TryParse(Console.ReadLine(), out grade) || grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Invalid input. Please enter a number between 0 and 100.");
+                Console.Write("Enter numerical grade: ");
+            }
+
+string letterGrade;
+    if (grade >= 88){
+        letterGrade = "A";   
+    }
+    else if (grade >= 80){
+        letterGrade = "B";
+    }
+    else if (grade >= 68){
+        letterGrade = "C";
+    }
+    else if (grade >= 60){
+        letterGrade = "D";
+    }
+    else{
+        letterGrade = "F";
+    }
+ 
+Console.WriteLine("Letter grade: "+ letterGrade);
+Console.WriteLine("Continue? (y/n): ");
+choice = Console.ReadLine().ToLower();
+}
+Console.WriteLine("Goodbye!");
+    }
+}
