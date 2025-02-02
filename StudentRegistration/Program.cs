64ebe26571f8cfe1bdf50fa538aa4fccd8e7b57c
csaksith <saksitca@mail.uc.edu>
2025-02-01 21:02:25 -0500

    complete project 2-1 student registration

--- StudentRegistration/Program.cs
+++ StudentRegistration/Program.cs
@@ -1,7 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Enter first name: ");
-firstName = Console.ReadLine();
+string firstName = Console.ReadLine();
+
 Console.WriteLine("Enter last name: ");
-lastName = Console.ReadLine();
+string lastName = Console.ReadLine();
+
 Console.WriteLine("Enter year of birth: ");
+int YOB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Welcome "+ firstName+ " "+ lastName+ '!');
+Console.WriteLine("Your registration is complete");
+Console.WriteLine("Your temporary password is: " + firstName+'*'+YOB);
 
