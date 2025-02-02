f55e88ebfc1525d02ac671968887be6c6ca835f2
csaksith <saksitca@mail.uc.edu>
2025-02-01 20:39:48 -0500

    add while loops, if else example

--- Ch04DemosNumbersStrings/Program.cs
+++ Ch04DemosNumbersStrings/Program.cs
@@ -34,9 +34,110 @@ double c = 9.0;
 double d = 2.0;
 //decimal math
 result = c / d;
-
 Console.WriteLine("result = " + result);
+
+int counter = 10;
+//increment by 1
+counter = counter + 1;
+counter++;
+counter+=1;
+Console.WriteLine("counter = "+counter);
+
+//add 5 to counter
+counter = counter + 5;
+counter += 5;
+
+//subtract 2 from counter
+counter -=2;
+
+
+//casting converting integer to a double
+int e = 5;
+double f = e;
+
+double g = 6.7;
+int h = (int)g; //explicit cast force the cast from decimal to integer 
+Console.WriteLine("h = " + h);
+
+// Math class
+interestRate = .0347825;
+Console.WriteLine("interestRate = "+interestRate);
+//rounded to 4 decimal places
+interestRate = Math.Round(interestRate,4);
+Console.WriteLine("interestRate = "+interestRate);
+
+//random number class
+Random rand = new Random(); //have to create an instance of a random
+int r1 = rand.Next();
+Console.WriteLine("r1 = "+ r1);
+int dieRoll = rand.Next(1,7);
+Console.WriteLine("dieRoll = "+ dieRoll);
+
+
+//strings
+string firstName = "Bob";
+string lastName = "Marley";
+string fullName = firstName + " "+lastName;
+
+
+//escape sequences - tab, newLine, backslash
+string sentence = "abc\tdef\nghi";
+Console.WriteLine("sentence = "+sentence);
+string reposFolder = "c:\\repos";
+Console.WriteLine("repos = "+reposFolder);
+
+string rawMessage = """
+hello
+nice to meet you
+you too
+""";
+Console.WriteLine(rawMessage);
+
+//get input from the console
+
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+
+
+
+//BASIC while loop
+//execute the 2 numbers and sum until user is done
+string choice = "y";
+while (choice == "y") {
+Console.WriteLine("Enter two whole number: ");
+int n1 = Convert.ToInt32(Console.ReadLine()); //method chaining from string to integer
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("sum = " + (n1 + n2));
+Console.WriteLine("Continue? (y/n): ");
+choice = Console.ReadLine();
+//the loop will continue forever until the choice is set to "n"
+}
+
+//if statement
+//discount percent
+//if amount > 1000, discPct = .10
+//else if amount > 800, discPct = .08
+//else if amount > 500, discPct = .05
+//otherwise discPct = .00
+double amount = 900;
+double discPct = 0.0;
+if (amount >= 1000) {
+        discPct = .1;
+}
+else if (amount >=800) {
+        discPct = .08;
+}
+else if (amount >=500) {
+        discPct = .05;
+}
+else {
+        discPct = 0.0; //technically you don't need this because we already defined it 
+}
+
 
 
 Console.WriteLine("Goodbye");
+
+
 
