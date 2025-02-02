1030b04837903f853142c236a4a8517609fc242d
csaksith <saksitca@mail.uc.edu>
2025-02-01 20:42:11 -0500

    init commit student registration ex 1

---
+++ StudentRegistration/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter first name: ");
+firstName = Console.ReadLine();
+Console.WriteLine("Enter last name: ");
+lastName = Console.ReadLine();
+Console.WriteLine("Enter year of birth: ");
+
