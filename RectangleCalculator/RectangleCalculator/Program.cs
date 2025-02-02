15949247227170f849285e40daddf977f85744f0
csaksith <saksitca@mail.uc.edu>
2025-02-02 15:47:23 -0500

    git init

---
+++ RectangleCalculator/RectangleCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Area and Perimeter Calculator");
+
+        
+        Console.WriteLine("Goodbye!");
+    }
+}
