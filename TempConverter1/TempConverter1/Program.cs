e7b6e6f7154327c14c79453ffd9804ea4c3311b9
csaksith <saksitca@mail.uc.edu>
2025-02-03 15:53:26 -0500

    complete temp converter exercise

---
+++ TempConverter1/TempConverter1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to the Temperature Converter");
+
+string choice = "y"; 
+while (choice == "y"){
+    Console.WriteLine("Enter degrees in Fahrenheit: ");
+    double tempF = double.Parse(Console.ReadLine());
+    double tempC = (tempF - 32)*(5.0/9);
+    Console.WriteLine("Degrees in Celsius: " + tempC.ToString("0.00"));  
+     Console.WriteLine("Continue? (y/n): ");
+     choice = Console.ReadLine();
+   }
+
+Console.WriteLine("Goodbye!");
