6a5a37a542b529e2b7f09f7a053c160ddd843585
csaksith <saksitca@mail.uc.edu>
2025-02-03 14:19:56 -0500

    add ch5 demos

---
+++ GradeConverterDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GradeConverterDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Letter Grade Converter");
+
+            String choice = "y";
+
+            // upper or lowercase y is ok
+            //while (choice.ToLower() == "y")
+            while (choice == "y" || choice== "Y")
+            {
+                // prompt for input - numericGrade: int
+                Console.Write("Enter numeric grade: ");
+                // int numericGrade = Convert.ToInt32(Console.ReadLine());
+                int numericGrade = Int32.Parse(Console.ReadLine());
+                // do business logic
+                string letterGrade = "F"; //do not need to do else F because already defaulted to "F"
+                // display result - display letterGrade
+                if (numericGrade >= 88)
+                    letterGrade = "A";
+                else if (numericGrade >= 80)
+                    letterGrade = "B";
+                else if (numericGrade >= 68)
+                    letterGrade = "D";
+                else if (numericGrade >= 60)
+                    letterGrade = "D";
+
+                Console.WriteLine("Letter grade: " +letterGrade);
+                Console.Write("Continue? (y/n): ");
+                choice = Console.ReadLine();
+
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}
---
+++ RectangleCalcDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RectangleCalcDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Rectangle Calculator App!");
+
+            // get input - length: double, width: double
+            Console.Write("Enter length: ");
+            double length = Double.Parse(Console.ReadLine());
+            Console.Write("Enter width: ");
+            double width = Double.Parse(Console.ReadLine());
+            // do business logic
+            // - calc area: length*width
+            // - calc perimeter: (2*length)+(2*width)
+            double area = length * width;
+            double perimeter = 2 * length + 2 * width; 
+            // display output
+            // - output area and perimeter
+            Console.WriteLine("Area: "+area);
+            Console.WriteLine("Perimeter: " + perimeter);
+
+
+
+            Console.WriteLine("Bye!");
+        }
+    }
+}
