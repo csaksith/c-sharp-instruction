5bc667019eb7333b28e9f1659bebec5fd025abd3
csaksith <saksitca@mail.uc.edu>
2025-02-03 21:28:00 -0500

    add travel time calculator

--- TempConverter/Program.cs
+++ TempConverter/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to the Temperature Converter");
+            string choice = "y";
+            while (choice.ToLower() == "y")
+            {
+                // prompt for temp input in fahrenheit
+                Console.WriteLine("Enter degrees in Fahrenheit: ");
+                double tempF = double.Parse(Console.ReadLine());
+                // convert to celsius
+                double tempC = ((tempF - 32) * (5.0 / 9));
+                Console.WriteLine("Degrees in Celsius: " + tempC.ToString("0.00"));
+                Console.WriteLine("Continue? (y/n): ");
+                choice = Console.ReadLine();
+            }
+                Console.WriteLine("Goodbye");
+            }
         }
     }
-}
---
+++ TravelTimeCalc/TravelTimeCalc/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TravelTimeCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Travel Time Calculator");
+        string choice = "y";
+            while (choice == "y")
+            {
+                Console.WriteLine("Enter miles: ");
+                double miles = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter miles per hour: ");
+                double mph = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Estimated travel time");
+                Console.WriteLine("----------------------");
+
+                double totalTime = miles / mph;
+                int hours = (int)totalTime; // get whole number of hours
+                int minutes = (int)((totalTime - hours) * 60); // get the decimal part and convert to minutes
+                Console.WriteLine("Hours: " + hours);
+
+                // Get the remaining minutes after dividing miles by mph
+                Console.WriteLine("Minutes: " + minutes);
+
+                Console.WriteLine("Continue? (y/n): ");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye!!");
+        }
+    }
+}
