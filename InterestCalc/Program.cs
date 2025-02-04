3c395a2030d28188667ef9a240ec4ccc233bd827
csaksith <saksitca@mail.uc.edu>
2025-02-03 21:57:51 -0500

    complete interest calc exercise

---
+++ InterestCalc/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace InterestCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Interest Calculator");
+
+            string choice = "y";
+            while (choice == "y")
+            {
+                Console.WriteLine("Enter loan amount: ");
+                double loanAmt = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter interest rate: ");
+                decimal intRate = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Loan amount: " + loanAmt);
+                Console.WriteLine("Interest rate: " + (intRate * 100));
+                double interest = loanAmt * (double)intRate;
+                Console.WriteLine("Interest: " + interest);
+                Console.WriteLine("Continue? (y/n): ");
+                choice = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
