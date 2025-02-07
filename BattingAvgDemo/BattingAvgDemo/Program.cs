8b49ef65f11945ddd00f5f9a8223a225ce51919f
csaksith <saksitca@mail.uc.edu>
2025-02-07 10:42:59 -0500

    add arrays exercise demos

---
+++ BattingAvgDemo/BattingAvgDemo/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace BattingAvgDemo {
+    internal class Program {
+        static void Main(string[] args) {
+            Print("Welcome to the Batting Average Caclulator!\n");
+
+            // while choice = "y"
+            string choice = "y";
+            while (choice=="y") {
+                int nbrAtBat = PromptInt("Enter number of times at bat: ",1,30);
+                // create array => atBat: int[nbrAtBat]
+                int[] atBat = new int[nbrAtBat];
+                int countAtBat = 0;
+                int sumAtBat = 0;
+                // for i = 0; i<nbrAtBat; i++ -- always start at 0 for an array
+                for (int i = 0; i<nbrAtBat; i++) {
+                    // prompt user => nbrAtBat: int
+                    // prompt user => result: int (result for atBat)
+                    int result = PromptInt("Result for at-bat: "+(i+1)+": ",0,4);
+                    atBat[i]=result;
+                    if (result>0) {
+                        countAtBat++;
+                    }
+                    sumAtBat+=result;
+                }
+                // biz logic:
+                // battingAvg: double = (count atBat>0) / nbrAtBat
+                // sluggingPct: double = (atBatSum) / nbrAtBat
+                double battingAvg = (double)countAtBat/nbrAtBat; // decimals typically for currency
+                double sluggingPct = (double)sumAtBat/nbrAtBat;
+                // display results - formatting: 3 decimal places
+                Print("\nBatting Average: "+battingAvg.ToString("F3"));
+                Print("Slugging Percent: "+sluggingPct.ToString("F3"));
+                // prompt another player?
+                Console.Write("Another player? (y/n): ");
+                choice=Console.ReadLine().ToLower();
+            }
+            Print("Goodbye!~");
+        }
+
+
+
+        static int PromptInt(string prompt,int min,int max) {
+            int result = 0;
+            bool isValid = false;
+
+            while (!isValid) {
+                // get a valid input -- TryParse?
+                // declare variable first (result)
+                Console.Write(prompt);
+                if (Int32.TryParse(Console.ReadLine(),out result)) {
+                    // if valid - validate in range
+                    if (result>=min&&result<=max) {
+                        isValid=true;
+                    }
+                    else {
+                        Print("Error: must be within range.");
+                    }
+                }
+                else {
+                    // if not valid - display a message
+                    Print("Error: entry must be an integer.");
+                }
+            }
+            return result;
+        }
+        
+
+        static void Print(string message) {
+            Console.WriteLine(message);
+        }
+
+
+    }
+}
---
+++ SalesReport/SalesReport/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace SalesReport {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("The Sales Report Application");
+
+            // for project 8-2 Sales Report
+            double[,] sales = { {1540.0, 2010.0, 2450.0, 1845.0},
+                                {1130.0, 1168.0, 1847.0, 1491.0},
+                                {1580.0, 2305.0, 2710.0, 1284.0},
+                                {1105.0, 4102.0, 2391.0, 1576.0}};
+
+            // grid 5 x 5
+            // Region   Q1  Q2  Q3  Q4
+            // 1
+            // 2
+            // 3
+            // 4
+            string[,] grid = new string[5,5];
+            grid[0,0]="Region";
+            grid[0,1]="Q1";
+            grid[0,2]="Q2";
+            grid[0,3]="Q3";
+            grid[0,4]="Q4";
+            grid[1,0]="1";
+            grid[2,0]="2";
+            grid[3,0]="3";
+            grid[4,0]="4";
+
+            // print grid for titles;
+            string gridString = "";
+            for (int i = 0; i<grid.GetLength(0); i++) {
+                for (int j = 0; j<grid.GetLength(1); j++) {
+                    gridString+=grid[i,j]+"\t";
+                }
+                gridString+="\n";
+            }
+            // add sales data into the grid
+            for (int i = 0; i<sales.GetLength(0); i++) {
+                for (int j = 0; j<sales.GetLength(1); j++) {
+                    grid[i+1,j+1]=sales[i,j].ToString("C");
+                }
+            }
+
+            for (int i = 0;i<sales.GetLength(0); i++) {
+                for (int j = 0; j<sales.GetLength(1); j++) {
+                    Console.Write(grid[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Goodbye!~");
+
+        }
+    }
+}
