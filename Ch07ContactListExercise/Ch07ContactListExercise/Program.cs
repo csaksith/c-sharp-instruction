88cab25b9697f465bf6397ba0b5516651283ec14
csaksith <saksitca@mail.uc.edu>
2025-02-10 21:32:59 -0500

    add ch 7 contact list exercise

---
+++ Ch07ContactListExercise/Ch07ContactListExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Ch07ContactListExercise {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.Print("Welcome to the Contact List App!~ \n");
+            MyConsole.PrintLine("");
+            string choice = "y";
+            while (choice=="y") {
+                string firstName = MyConsole.PromptString("Enter first name: ");
+                string lastName = MyConsole.PromptString("Enter last name: ");
+                string email = MyConsole.PromptString("Enter email: ");
+                string phonenum = MyConsole.PromptString("Enter phone: ");
+                MyConsole.PrintLine("");
+                MyConsole.PrintLine("--------------------------------------------");
+                MyConsole.PrintLine("---- Current Contact -----------------------");
+                MyConsole.PrintLine("--------------------------------------------");
+                MyConsole.Print("Name: \t \t");
+                MyConsole.Print(firstName+" "+lastName);
+                MyConsole.Print("\nEmail: \t \t");
+                MyConsole.Print(email);
+                MyConsole.Print("\nPhone Number: \t");
+                MyConsole.Print(phonenum);
+                MyConsole.PrintLine("\n--------------------------------------------");
+                MyConsole.PrintLine("");
+                choice = MyConsole.PromptString("Continue? (y/n): ");
+            }
+
+
+
+
+            MyConsole.Print("Goodbye!~");
+        }
+    }
+}
