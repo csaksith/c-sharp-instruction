bba98de6ee6320c24b61f0af6d346184a6fd9022
csaksith <saksitca@mail.uc.edu>
2025-02-10 22:31:56 -0500

    add ch 7 grade converter hw

---
+++ Ch7GradeConverter/Ch7GradeConverter/MyConsole.cs
@@ -0,0 +1,28 @@
+﻿namespace Ch07GradeConverter{
+    public class MyConsole {
+        public static int PromptInt(string prompt) {
+            int result = 0;
+            bool isValid = false;
+            while (!isValid) {
+                Print(prompt);
+                if (Int32.TryParse(Console.ReadLine(),out result)) {
+                    isValid=true;
+                }
+                else {
+                    PrintLine("Error: invalid integer");
+                }
+            }
+            return result;
+        }
+        public static string PromptString(string prompt) {
+            Print(prompt);
+            return Console.ReadLine();
+        }
+        public static void Print(string message) {
+            Console.Write(message);
+        }
+        public static void PrintLine(string message) {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Ch7GradeConverter/Ch7GradeConverter/Program.cs
@@ -0,0 +1,44 @@
+﻿using Ch07GradeConverter;
+
+namespace Ch7GradeConverter {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to the Letter Grade Converter~!");
+            string choice = "y";
+            while (choice=="y") {
+                int numGrade = MyConsole.PromptInt("Enter numerical grade: ");
+                if (numGrade>88) {
+                    string letterGrade = "A";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else if (numGrade>=80) {
+                    string letterGrade = "B";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else if (numGrade>=67) {
+                    string letterGrade = "C";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else if (numGrade>=60) {
+                    string letterGrade = "D";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else if (numGrade<60) {
+                    string letterGrade = "D";
+                    MyConsole.PrintLine($"Letter Grade: {letterGrade}");
+                }
+                else {
+                    MyConsole.PrintLine("Error! This entry is required. Try again.");
+                }
+
+                choice=MyConsole.PromptString("Continue? (y/n): ");
+                while (choice!="y"&&choice!="n") {
+                    MyConsole.PrintLine("Error! Entry must be 'y' or 'n'. Try again.");
+                choice=MyConsole.PromptString("Continue? (y/n): ");
+
+            }
+        }
+        MyConsole.PrintLine("Goodbye!");
+        }
+}
+}
