19d274c8729751a245d7fe4f0424406766cce940
csaksith <saksitca@mail.uc.edu>
2025-02-12 12:29:33 -0500

    add Chapter 12 Classes Demo

--- Ch12ClassesDemo/Ch12ClassesDemo/Program.cs
+++ Ch12ClassesDemo/Ch12ClassesDemo/Program.cs
@@ -1,47 +1,51 @@
 ﻿namespace Ch12ClassesDemo {
     internal class Program {
-        static List<Movie> Movies = new List<Movie>();
+        
         static void Main(string[] args) {
             Console.WriteLine("Welcome to Chapter 12 - Class Demo!");
+
             // capture input for properties of a movie
+            string title = PromptString("Movie Title: ");
+            int year = PromptInt("Year: ");
+            string ageRating = PromptString("Age Rating: ");
+            string director = PromptString("Director: ");
 
-            Movie newMovie = new Movie();
-            newMovie.Id=1;
-            newMovie.Title="Jurassic Park";
-            newMovie.Year=2025;
-            newMovie.Rating="PG-13";
-            newMovie.Director="Gareth Edwards";
-            Movies.Add(newMovie);
-            string choice = "y";
-            while (choice=="y") {
-                int id = MyConsole.PromptInt("Movie ID: ");
-                string title = MyConsole.PromptString("Movie Title: ");
-                int year = MyConsole.PromptInt("Year: ");
-                string ageRating = MyConsole.PromptString("Age rating: ");
-                string director = MyConsole.PromptString("Director: ");
-                // store this movie and capture a new one
-                newMovie=new Movie(id,title,year,ageRating,director);
-                Movies.Add(newMovie);
-                choice=MyConsole.PromptString("Add another movie? (y/n): ");
-                // repeat until user is done entering movies
-            }
-            // display all movies
-            // getter
+            Movie newMovie = ?????
 
-            ////setter
-            //PrintLine($"newMovie title = {newMovie.Title}");
-            //PrintLine(newMovie.ToString());
-
-
-            // store this movie and capture a new one
+            // stpre this movie and capture a new one
             // repeat until user is done entering movies
             // display all movies
-            MyConsole.PrintLine("========== MOVIES ========");
-            foreach (Movie movie in Movies) {
-                MyConsole.PrintLine(movie.ToString());
-            }
+
+
             Console.WriteLine("BYE!");
         }
-       
+
+        static int PromptInt(string prompt){
+            int result = 0;
+            bool isValid = false;
+            while (!isValid) {
+                Print(prompt);
+                if (Int32.TryParse (Console.ReadLine(),out result)){
+                    isValid = true;
+                }
+                else{
+                    PrintLine("Error - invalid int");
+                }
+            }
+            return result;
+        }
+
+        static string PromptString(string prompt) {
+            Print(prompt);
+            return Console.ReadLine();
+        }
+
+       static void Print(string msg){
+            Console.Write(msg);
+        }
+
+        static void PrintLine(string msg){
+            Console.WriteLine(msg);
+        }
     }
 }
