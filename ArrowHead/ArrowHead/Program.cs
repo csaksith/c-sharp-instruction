0f88fc4e799a989af327809348d7532f18b9e947
csaksith <saksitca@mail.uc.edu>
2025-02-14 11:37:54 -0500

    git init arrowhead coding challenge

---
+++ ArrowHead/ArrowHead/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ArrowHead
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Arrowhead generator!~");
+            Console.Write("Enter the size of arrowhead: ");
+            int aSize = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i<aSize; i++) {
+                Console.WriteLine(">");
+            }
+            Console.WriteLine("Goodbye~");
+        }
+    }
+}
