65600fa19819a82bb58d392a7b39de68f1b26183
csaksith <saksitca@mail.uc.edu>
2025-02-16 16:29:58 -0500

    git inti c sharp practice

---
+++ csharp02162025practice/csharp02162025practice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp02162025practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
