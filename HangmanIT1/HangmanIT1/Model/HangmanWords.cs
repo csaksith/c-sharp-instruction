dc5e9a7bd9c1a35db8756a19160d321a2fd55355
csaksith <saksitca@mail.uc.edu>
2025-02-18 14:13:43 -0500

    add iteration 1,2,3

---
+++ HangmanIT1/HangmanIT1/Model/HangmanWords.cs
@@ -0,0 +1,17 @@
+﻿namespace HangmanIT1.Model {
+    internal class HangmanWords {
+        // declare dictionary of words
+        private List<string> words;
+        public HangmanWords() {
+        words = new List<string> { "pizza","sushi","steak","pasta","tacos" };
+        }
+
+        public string GetRandomWord() {
+            Random random = new Random();
+            int index = random.Next(words.Count);
+            return words[index];
+        }
+    }
+   
+}
+  
---
+++ HangmanIT1/HangmanIT1/Program.cs
@@ -0,0 +1,108 @@
+﻿using ConsoleLibrary;
+using HangmanIT1.Model;
+
+namespace HangmanIT1 {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to Hangman ~ ");
+            MyConsole.PrintLine("=====================\n");
+
+            // create an instance of HangmanWords
+            HangmanWords wordGenerator = new HangmanWords();
+            // get random word using getrandomword method
+            string randomWord = wordGenerator.GetRandomWord();
+            MyConsole.Print($"Random Word: {randomWord}");
+            // create for loop to display hangman stages 1-6
+            //MyConsole.PrintLine("Testing hangman stages: \n");
+            //for (int i = 0; i<=6; i++) {
+            //    MyConsole.PrintLine($"Incorrect Guesses: {i}");
+            //    MyConsole.PrintLine(GetHangmanImage(i));
+            //    MyConsole.PrintLine(""); 
+
+            //}
+            MyConsole.PrintLine("\nBye~! ");
+            Console.ReadLine();
+        }
+        static string[] hangmanStages = new string[] {
+            // iteration 1 - drawing the hangman
+            // using raw string literals
+            // stage 0 - no incorrect answers
+            """
+            -----
+            |   |
+            |
+            |
+            |
+            |
+            ------
+            """, 
+            // stage 1 - 1 incorrect answer
+            """
+            -----
+            |   |
+            |   O
+            |
+            |
+            |
+            ------
+            """,
+             // stage 2 - 2 incorrect answers
+            """
+            -----
+            |   |
+            |   O
+            |   |
+            |
+            |
+            ------
+            """,
+             // stage 3 - 3 incorrect answers
+            """
+            -----
+            |   |
+            |   O
+            |  /|
+            |
+            |
+            ------
+            """,
+             // stage 4 - 4 incorrect answers
+            """
+            -----
+            |   |
+            |   O
+            |  /|\
+            |
+            |
+            ------
+            """,
+             // stage 5 - 5 incorrect answers
+            """
+            -----
+            |   |
+            |   O
+            |  /|\
+            |  /
+            |
+            ------
+            """,
+             // stage 6 - 6 incorrect answers
+            """
+            -----
+            |   |
+            |   O
+            |  /|\
+            |  / \
+            |
+            ------
+            """ };
+        public static string GetHangmanImage(int incorrectGuesses) {
+            
+            if (incorrectGuesses<0||incorrectGuesses>=hangmanStages.Length) {
+                return "Invalid number of incorrect guesses!";
+            }
+            return hangmanStages[incorrectGuesses];
+
+        }
+    }
+}
