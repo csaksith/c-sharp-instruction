a49b40358d13bc8b58bbae592d19969ffd91832c
csaksith <saksitca@mail.uc.edu>
2025-02-20 13:50:58 -0500

    add hangman game attempt 2

---
+++ HangmanIT2/HangmanIT2/Model/HangmanWords.cs
@@ -0,0 +1,51 @@
+﻿namespace HangmanIT2.Model {
+    internal class HangmanWords {
+        // intialize list words
+        public List<string> words;
+        // constructor for list words
+        public HangmanWords() {
+            words=new List<string> { "banana","elephant","guitar","puzzle","octopus","mountain","sunflower","chocolate","penguin","butterfly" };
+
+        }
+        // method to get random word off list
+        // SB QUESTION *** using static I GET THIS ERROR An object reference is required for the non-static field, method, or property 'HangmanWords.words'
+        public string GetRandomWord() {
+            Random random = new Random();
+            // using index to randomize the word array to return
+            int index = random.Next(words.Count);
+            return words[index];
+        }
+        // make word into only underlines 
+        public string GetHiddenWord(string words) {
+            // start with blank string for hidden word
+            string hiddenWord = "";
+            // create a for loop to print the amount of _ and spaces depending on the length of the word
+            for (int i = 0; i<words.Length; i++) {
+                hiddenWord+="_ ";
+            }
+            return hiddenWord;
+        }
+        // iteration 5
+        // method to reveal hidden word by correct letter
+        // checks if the guessed letter exists in 'word'
+        // replaces the corresponding underscore in 'hiddenWord' with the guessed letter
+        // returns the updated hiddenWord
+        public string DisplayHiddenWord(string words,string hiddenWord,char guessedLetter) {
+            // convert the hidden word (underscores) into char array
+            // removes the spaces and also converts to character array
+            char[] updatedHiddenWord = hiddenWord.Replace(" ","").ToCharArray();
+
+            // loop through each letter of actual word
+            for (int i = 0; i<words.Length; i++) {
+                // if guess letter matches the actual word - replace underscore with letter
+                if (words[i]==guessedLetter) {
+                    updatedHiddenWord[i]=guessedLetter; // replace with letter
+                }
+            }
+            // convert char array back to string with spaces in between letters
+            return string.Join(" ",updatedHiddenWord);
+
+        }
+    }
+}
+
---
+++ HangmanIT2/HangmanIT2/Program.cs
@@ -0,0 +1,169 @@
+﻿using ConsoleLibrary;
+using HangmanIT2.Model;
+
+namespace HangmanIT2 {
+
+    internal class Program {
+
+        private static void Main(string[] args) {
+            MyConsole.PrintLine("~Welcome to Hangman!~\n");
+
+            //// iteration 2: testing GetHangmanMethod
+            //int incorrectGuesses = 0;
+            //MyConsole.PrintLine("Testing GetHangmanMethod");
+            //// for loop to run through all stages
+            //for (int i = 0; i<=6; i++) {
+            //    MyConsole.PrintLine($"incorrectGuesses - Hangman Stage {i}");
+            //    MyConsole.PrintLine(GetHangmanImage(i));
+            //    MyConsole.PrintLine();
+            //}
+
+            // iteration 6 setting up loop for game
+            // while loop for string choice 
+            string choice = "y";
+            while (choice=="y") {
+                // creating an instance of HangmanWords class
+                // object allows you to call methods from HangmanWords class
+                HangmanWords wordGenerator = new HangmanWords();
+                string word = wordGenerator.GetRandomWord();
+                MyConsole.PrintLine($"\nDebug Purposes - word is {word}.\n");
+                string hiddenWord = wordGenerator.GetHiddenWord(word);
+                MyConsole.PrintLine(hiddenWord);
+                // while loop for guessing word
+                bool correctWord = false;
+                string guessLetter = "";
+                int incorrectGuess = 0;
+                while (!correctWord&&incorrectGuess<6) {
+                    // user input guess letter
+                    guessLetter=MyConsole.PromptString("\nGuess a letter! (Choose wisely!): ");
+                    // loop through "word" if character matches "guessLetter"
+                    // if word contains guess letter
+                    if (word.Contains(guessLetter)) {
+                        // convert hiddenWord into character array
+                        hiddenWord=wordGenerator.DisplayHiddenWord(word,hiddenWord,guessLetter[0]);
+                        MyConsole.PrintLine(hiddenWord);
+                    }
+                    if (!hiddenWord.Contains("_")) {
+                        correctWord=true;
+                        MyConsole.PrintLine("\n~Congrats girly you won~");
+                    }
+                   
+                    else if (!word.Contains(guessLetter)) { 
+                        incorrectGuess++;
+                        MyConsole.PrintLine($"\nYikes you have {6-incorrectGuess} out of 6 guesses left!");
+                        MyConsole.PrintLine(hangmanImage[incorrectGuess]);
+                    }
+                    if (incorrectGuess==6) {
+                        MyConsole.PrintLine("\nYikes you lost, try again!");
+                        MyConsole.PrintLine($"The correct word was {word}.");
+                    }
+                }
+                // if word does not guess letter 
+                // increment guess count
+                // display hangman
+                // choice = continue? (y/n)
+                choice=MyConsole.PromptReqString("Play again? (y/n): ","y","n");
+            }
+            // iteration 3: testing instance of random word
+            // initialize object
+            // declare variable 'wordGenerator' of type HangmanWords
+            // new HangmanWords() calls the constructor HangmanWords class, 
+            // creating a new instance (object) of the class 
+            //HangmanWords wordGenerator = new HangmanWords();
+            //// GetRandomWord method is applied to wordGenerator object then assigned to the word "variable
+            //string word = wordGenerator.GetRandomWord();
+            //MyConsole.PrintLine(word);
+            //// iteration 4 - revealing correct letter and replacing underscores
+            //string hiddenWord = wordGenerator.GetHiddenWord(word);
+            //MyConsole.PrintLine(hiddenWord);
+
+
+
+            MyConsole.PrintLine("\nSee you next time!~");
+        }
+
+        private static string[] hangmanImage = new string[] {
+                // 0
+                """
+                -----
+                |   |
+                |
+                |
+                |
+                |
+                -------
+                """,
+                // 1
+                """
+                -----
+                |   |
+                |   O
+                |
+                |
+                |
+                -------
+                """,
+                // 2
+                """
+                -----
+                |   |
+                |   O
+                |   |
+                |
+                |
+                -------
+                """,
+                // 3
+                """
+                -----
+                |   |
+                |   O
+                |  /|
+                |
+                |
+                -------
+                """,
+                // 4
+                """
+                -----
+                |   |
+                |   O
+                |  /|\
+                |
+                |
+                -------
+                """,
+                // 5
+                """
+                -----
+                |   |
+                |   O
+                |  /|\
+                |  /
+                |
+                -------
+                """,
+                // 6
+                """
+                -----
+                |   |
+                |   O
+                |  /|\
+                |  / \
+                |
+                -------
+                """,
+            };
+
+        // GetHangmanImage method
+        // if statement of display hangman depending on the number of incorrect guesses.
+        public static string GetHangmanImage(int incorrectGuesses) {
+            if (incorrectGuesses<=hangmanImage.Length) {
+                return hangmanImage[incorrectGuesses];
+            }
+            else {
+                return "Invalid number of incorrect guesses!";
+            }
+        }
+    }
+}
