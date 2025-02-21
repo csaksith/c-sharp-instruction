21c7d0652b956ff98423d3551936cb9b7d6eb062
csaksith <saksitca@mail.uc.edu>
2025-02-21 09:29:40 -0500

    git init ch 8 country list manager

---
+++ Ch8CountryListManagerPractice/Ch8CountryListManagerPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ch8CountryListManagerPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
