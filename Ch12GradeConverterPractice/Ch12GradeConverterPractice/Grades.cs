a22823366dec8a57473cc90a84e497297adffc9f
csaksith <saksitca@mail.uc.edu>
2025-02-20 20:07:55 -0500

    add practice run of grade converter

---
+++ Ch12GradeConverterPractice/Ch12GradeConverterPractice/Grades.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Ch12GradeConverterPractice {
+    internal class Grades {
+        // initialize numGrade variable 
+        public int numGrade { get; set; }
+        // constructor for Grades
+        public Grades(int numGrade) {
+            this.numGrade=numGrade;
+        }
+        // constructor to convert numGrade to letterGrade
+        // must have return variable
+        public string GradeConverter() {
+            string letterGrade = "F";
+            // if statements for grades
+            if (numGrade>=88) {
+                letterGrade="A";
+            }
+            else if (numGrade>=80) {
+                letterGrade="B";
+            }
+            else if (numGrade>=68) {
+                letterGrade="C";
+            }
+            else if (numGrade>=60) {
+                letterGrade="D";
+            }
+                return letterGrade;
+        }
+
+    }
+}
---
+++ Ch12GradeConverterPractice/Ch12GradeConverterPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleLibrary;
+
+namespace Ch12GradeConverterPractice {
+    internal class Program {
+        static void Main(string[] args) {
+            MyConsole.PrintLine("Welcome to the Letter Grade Converter!!~\n");
+
+            string choice = "y";
+            while (choice=="y") {
+                // user inputs numerical grade
+                int numGrade = MyConsole.PromptInt("Enter numerical grade: ",0,100);
+                // create instance of Grade class 
+                Grades grade = new Grades(numGrade);
+                // display letter grade 
+                MyConsole.PrintLine("Letter grade: "+grade.GradeConverter());
+                // continue? (y/n):
+                choice=MyConsole.PromptReqString("\nContinue? (y/n): ","y","n");
+            }
+            MyConsole.PrintLine("Goodbye for now!!~");
+        }
+    }
+}
