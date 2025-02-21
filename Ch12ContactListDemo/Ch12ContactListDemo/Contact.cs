812f8411434ac7588fda1535884f2687f2d696b9
csaksith <saksitca@mail.uc.edu>
2025-02-21 08:13:27 -0500

    add ch12 contact list class demo

---
+++ Ch12ContactListDemo/Ch12ContactListDemo/Contact.cs
@@ -0,0 +1,31 @@
+﻿namespace Ch12ContactListDemo {
+    internal class Contact {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public Contact(string FirstName,string LastName,string Email,string Phone) {
+            this.FirstName=FirstName;
+            this.LastName=LastName;
+            this.Email=Email;
+            this.Phone=Phone;
+            this.Phone.ToString();
+        }
+
+        public string DisplayContact() {
+            string displayStr = "-------------------------------\n";
+            displayStr+="----------- Current Contact -----------\n";
+            displayStr += "-------------------------------\n";
+            displayStr+= "Name:              "+this.FirstName+" "+ this.LastName +"\n";
+            displayStr+= "Email Address:       "+ this.Email +"\n";
+            displayStr+= "Phone Number:        "+ this.Phone +"\n";
+            displayStr += "-------------------------------\n";
+            return displayStr;
+
+            // class that contacts all the variables that make up in a content in our address book
+
+
+        }
+    }
+}
---
+++ Ch12ContactListDemo/Ch12ContactListDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ch12ContactListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Welcome to Ch 12 Contact List ~ DEMO ~\n");
+            string choice = "y";
+            while (choice=="y") {
+                // prompt firstName, lastName, email, phone
+                string firstName = MyConsole.PromptString("Enter first name: ");
+                string lastName = MyConsole.PromptString("Enter last name: ");
+                string email = MyConsole.PromptString("Enter email: ");
+                string phone = MyConsole.PromptString("Enter phone: ");
+                // create a contact instance
+                //  instance is a specific object created from a class, with its own unique data.
+                Contact C = new Contact(firstName, lastName, email, phone);
+                // displayContact - call it by the instance of the class/
+                MyConsole.PrintLine(C.DisplayContact());
+                // prompt continue
+                choice=MyConsole.PromptString("Continue? (y/n): ").ToLower().Trim();
+            }
+            MyConsole.PrintLine("Goodbye~");
+
+        }
+    }
+}
