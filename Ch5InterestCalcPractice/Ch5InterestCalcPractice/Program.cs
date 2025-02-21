cc1717e24347f48e60c3fdcf93bf53b76cc82c21
csaksith <saksitca@mail.uc.edu>
2025-02-20 21:00:09 -0500

    add interest calc exercise

---
+++ Ch5InterestCalcPractice/Ch5InterestCalcPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleLibrary;
+
+namespace Ch5InterestCalcPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintLine("Welcome to the Interest Calculator ~\n");
+
+            string choice = "y";
+            while (choice=="y") {
+                // user input loan amount and interest rate
+                // display loan amount in $, interest rate in %, and interest payment in $
+                double loanAmt = MyConsole.PromptDouble("\nEnter loan amount: ");
+                decimal loanRate = MyConsole.PromptDecimal("Enter interest rate: ");
+                double loanPayment = (double)loanRate*loanAmt;
+                MyConsole.PrintLine($"\nLoan Amount:\t"+loanAmt.ToString("c"));
+                MyConsole.PrintLine($"Interest Rate:\t"+loanRate.ToString("p"));
+                MyConsole.PrintLine($"Interest:\t"+loanPayment.ToString("c"));
+
+
+                choice=MyConsole.PromptReqString("\nContinue? (y/n): ","y","n");
+            }
+            MyConsole.PrintLine("\nBye!");
+        }
+    }
+}
